7028b61f4daf2c49380d29d67e79dc229684ff9c
Krys112 <biomadkip@hotmail.co.uk>
2020-01-21 11:33:30 +0000

    Updated to use react hooks

--- src/context.js
+++ src/context.js
@@ -1,43 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Context = React.createContext();
+export const Context = React.createContext();
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'SEARCH_TRACKS':
-      return {
-        ...state,
-        track_list: action.payload,
-        heading: 'Search Results'
-      };
-    default:
-      return state;
-  }
-}
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'SEARCH_TRACKS':
+//       return {
+//         ...state,
+//         track_list: action.payload,
+//         heading: 'Search Results'
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
-export class Provider extends Component {
-  state = {
+export function ContextController({ children }) {
+  let initialState = {
     track_list: [],
-    heading: 'Top 10 Tracks',
-    dispatch: action => this.setState(state => reducer(state, action))
+    heading: "",
+    // dispatch: action => this.setState(state => reducer(state, action))
   };
 
-  componentDidMount() {
-    axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
       .then(res => {
-        this.setState({ track_list: res.data.message.body.track_list });
+        setState({
+          track_list: res.data.message.body.track_list,
+          heading: "Top 10 Tracks"
+        });
       })
       .catch(err => console.log(err));
-  }
+  }, []);
 
-  render() {
-    return (
-      <Context.Provider value={this.state}>
-        {this.props.children}
-      </Context.Provider>
-    )
-  }
+  return (
+    <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+  );
 }
-
-export const Consumer = Context.Consumer;
